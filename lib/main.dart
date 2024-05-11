e5b3cffe51f62ab197d062b08d61c4a656aac218
dabi2 <dabiangjalong3832gmail.com>
2024-05-11 15:41:35 +0530

    mm

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loanmobileapp/firebase_options.dart';
 import 'package:loanmobileapp/pages/Mybottomnavigationbar.dart';
 import 'package:loanmobileapp/pages/homePage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
 
